81f2916a7888011c917f56f13836e7d9e204041c
Uri Mikhli <urimikhli@gmail.com>
2014-04-02 13:38:51 -0400

    move the couponcode processing to after the 0 qty processing as suggested  by spree 1-3-stable commit 9a5d14a93c293105b964e1020a1117ff78c26850

--- promo/app/controllers/spree/orders_controller_decorator.rb
+++ promo/app/controllers/spree/orders_controller_decorator.rb
@@ -8,9 +8,9 @@ Spree::OrdersController.class_eval do
     end
 
     if @order.update_attributes(params[:order])
-      render :edit and return unless apply_coupon_code
       
       @order.line_items = @order.line_items.select {|li| li.quantity > 0 }
+      render :edit and return unless apply_coupon_code
       fire_event('spree.order.contents_changed')
       respond_with(@order) do |format|
         format.html do
